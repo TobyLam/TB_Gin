1d927e0fc372cf1385e8f19651038799e72e3dc7
TobyLam <toby1344319244@163.com>
2024-10-18 12:06:44 +0800

    feat:Gin 单文件上传、同名\不同名多文件上传

--- gindemo07/controllers/admin/userController.go
+++ gindemo07/controllers/admin/userController.go
@@ -3,6 +3,7 @@ package admin
 import (
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"path"
 )
 
 type UserController struct{
@@ -14,9 +15,73 @@ func (con UserController) Index(c *gin.Context) {
 }
 
 func (con UserController) Add(c *gin.Context) {
-	c.String(http.StatusOK,"新增用户~~~~")
+	//c.String(http.StatusOK,"新增用户~~~~")
+	c.HTML(http.StatusOK,"admin/useradd.html",gin.H{})
 }
 
-func (con UserController) Edit(c *gin.Context) {
-	c.String(http.StatusOK,"编辑用户")
+func (con UserController) DoUpload(c *gin.Context) {
+	username := c.PostForm("username")
+	file,err := c.FormFile("face")
+
+	// file.Filename 获取文件名称
+	dst := path.Join("./static/upload",file.Filename)
+	if err == nil {
+		c.SaveUploadedFile(file,dst)
+	}
+	//c.String(http.StatusOK,"执行上传")
+	c.JSON(http.StatusOK,gin.H{
+		"success":true,
+		"username":username,
+		"dst":dst,
+	})
+}
+
+func (con UserController) Edit(c *gin.Context){
+	c.HTML(http.StatusOK,"admin/useredit.html",gin.H{})
+}
+
+func (con UserController) DoEdit(c *gin.Context){
+	username := c.PostForm("username")
+	face1,err1 := c.FormFile("face1")
+	dst1 := path.Join("./static/upload",face1.Filename)
+	if err1 == nil{
+		c.SaveUploadedFile(face1,dst1)
+	}
+
+	face2,err2 := c.FormFile("face2")
+	dst2 := path.Join("./static/upload",face2.Filename)
+	if err2 == nil{
+		c.SaveUploadedFile(face2,dst2)
+	}
+
+	c.JSON(http.StatusOK,gin.H{
+		"success":true,
+		"username":username,
+		"dst1":dst1,
+		"dst2":dst2,
+	})
+
+}
+
+func (con UserController) Edit2(c *gin.Context){
+	c.HTML(http.StatusOK,"admin/useredit2.html",gin.H{})
+}
+
+func (con UserController) DoEdit2(c *gin.Context){
+	username := c.PostForm("username")
+
+	form,_ := c.MultipartForm()
+	files := form.File["face[]"]
+
+	for _,file := range files{
+		dst := path.Join("./static/upload",file.Filename)
+		//上传文件至指定目录
+		c.SaveUploadedFile(file,dst)
+	}
+
+	c.JSON(http.StatusOK,gin.H{
+		"success":true,
+		"username":username,
+	})
+
 }
--- gindemo07/routers/adminRouters.go
+++ gindemo07/routers/adminRouters.go
@@ -15,7 +15,11 @@ func AdminRoutersInit(r *gin.Engine) {
 		//方法不能加括号，表示注册方法，加了括号表示执行
 		adminRouters.GET("/user", admin.UserController{}.Index)
 		adminRouters.GET("/user/add", admin.UserController{}.Add)
+		adminRouters.POST("/user/doUpload", admin.UserController{}.DoUpload)
 		adminRouters.GET("/user/edit", admin.UserController{}.Edit)
+		adminRouters.POST("/user/doEdit", admin.UserController{}.DoEdit)
+		adminRouters.GET("/user/edit2", admin.UserController{}.Edit2)
+		adminRouters.POST("/user/doEdit2", admin.UserController{}.DoEdit2)
 
 		adminRouters.GET("/article", admin.ArticleController{}.Index)
 		adminRouters.GET("/article/add", admin.ArticleController{}.Add)
